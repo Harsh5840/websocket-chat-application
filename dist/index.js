e129b131f005fe809550accb479fff6e7cd91b53
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-03-26 01:49:37 +0530

    full logic implemented

--- dist/index.js
+++ dist/index.js
@@ -2,17 +2,26 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const wss = new ws_1.WebSocketServer({ port: 6000 });
-let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
-    allSockets.push(socket);
-    userCount += 1;
-    console.log("user connected #" + userCount);
     socket.on("message", (message) => {
-        console.log("message received" + message.toString());
-        for (let i = 0; i < allSockets.length; i++) {
-            const s = allSockets[i];
-            s.send(message.toString() + ":sent from server");
+        //@ts-ignore
+        const parsedMessage = JSON.parse(message); //converts string to an object
+        if (parsedMessage.type === "join") {
+            console.log("user joinde room" + parsedMessage.payload.roomId);
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            });
+        }
+        if (parsedMessage.type === "chat") {
+            const currentUser = allSockets.find((x) => x.socket == socket);
+            const currentUserRoom = currentUser ? currentUser.room : null;
+            for (let i = 0; i < allSockets.length; i++) {
+                if (allSockets[i].room === currentUserRoom) {
+                    allSockets[i].socket.send(parsedMessage.payload.message);
+                }
+            }
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ wss.on("connection",(socket) => {
         //@ts-ignore
         const parsedMessage = JSON.parse(message );  //converts string to an object
         if(parsedMessage.type === "join") {
+            console.log("user joinde room" + parsedMessage.payload.roomId);
             allSockets.push({
                 socket,
                 room: parsedMessage.payload.roomId
@@ -22,7 +23,8 @@ wss.on("connection",(socket) => {
         }
 
         if(parsedMessage.type === "chat"){
-            const currentUserRoom = allSockets.find((x) => x.socket == socket)?.room
+            const currentUser = allSockets.find((x) => x.socket == socket);
+            const currentUserRoom = currentUser ? currentUser.room : null;
             for (let i=0; i<allSockets.length; i++){
                 if(allSockets[i].room === currentUserRoom){
                         allSockets[i].socket.send(parsedMessage.payload.message)
