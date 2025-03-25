04325968d7f4821a69bf6291a87fcec35c787f37
D-E-V-Shukla <shuklaharsh5840@gmail.com>
2025-03-25 21:17:30 +0530

    logic implemented

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-import { WebSocket } from "ws";
+import { WebSocketServer } from "ws";
+ 
+const wss = new WebSocketServer ({port: 5000});
+
+let userCount = 0;
+
+wss.on("connection",(socket) => {
+
+    userCount += 1;
+    console.log("user connected #" + userCount);
+
+    socket.on("message" , (message) =>{
+            console.log("message received" + message.toString())
+            setTimeout(()=>{
+                socket.send(message.toString()) + ":sent from server"
+            } , 1000);
+    })
+}) 
